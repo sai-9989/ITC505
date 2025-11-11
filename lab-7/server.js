f26bc3de8d9a1ab1fc12aec25d526f2a8f11ecb6
sai-9989 <sm5654@nau.edu>
2025-11-11 01:27:43 -0700

    Delete lab-7 directory

--- lab-7/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.use(express.static(__dirname)); // Serve files from current directory
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
